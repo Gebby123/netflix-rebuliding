0d442937a7082ded2dc2cef17e807b691c400cd1
gebby123 <113621373+Gebby123@users.noreply.github.com>
2023-10-21 11:54:49 +0300

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    // return () => {
+    //   window.removeEventListener("scroll");
+    // };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dttps://images.unsplash.com/photo-1611798821136-26bfb61b734f?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+     
+        <h1 className="title"> cloned by Gebby</h1>
+     
+      <img
+        className="nav__avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Avatar logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
